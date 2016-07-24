e0b564b747dd8a2b5e4bc66c299f1b7478bc8cfb
Jonathan Hedstrom <delnorte91@gmail.com>
2016-07-24 11:24:11 -0500

    Initial Xcode project setup commit

---
+++ BloQuery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BloQuery
+//
+//  Created by Jonathan on 7/24/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BloQuery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BloQuery
+//
+//  Created by Jonathan on 7/24/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
