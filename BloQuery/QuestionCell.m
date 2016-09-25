0f59f1db7be27c5a41f44d247b2f0f8e7dd88b2b
Jonathan Hedstrom <delnorte91@gmail.com>
2016-09-25 16:55:11 -0500

    updated thumbs up to reflect number of answers

--- BloQuery/QuestionCell.m
+++ BloQuery/QuestionCell.m
@@ -52,9 +52,19 @@
     } else {
         self.numberOfAnswers.text = [NSString stringWithFormat:@"%ld Answers", (long) question.numberOfAnswers];
     }
-    //TODO: assign 1,2, or 3 thumbs up depending on how many likes there are
-    NSString *likeSeries = [NSString stringWithUTF8String:"\xF0\x9F\x91\x8D \xF0\x9F\x91\x8D"];
-    self.likeLabel.text = likeSeries;
+    // assign 1,2, or 3 thumbs up depending on how many likes there are
+    if (question.numberOfAnswers == 0) {
+        self.likeLabel.text = nil;
+    } else if (question.numberOfAnswers >= 1 && question.numberOfAnswers <= 3) {
+        NSString *likeSeries = [NSString stringWithUTF8String:"\xF0\x9F\x91\x8D"];
+        self.likeLabel.text = likeSeries;
+    } else if (question.numberOfAnswers > 3 && question.numberOfAnswers <= 10) {
+        NSString *likeSeries = [NSString stringWithUTF8String:"\xF0\x9F\x91\x8D \xF0\x9F\x91\x8D"];
+        self.likeLabel.text = likeSeries;
+    } else {
+        NSString *likeSeries = [NSString stringWithUTF8String:"\xF0\x9F\x91\x8D \xF0\x9F\x91\x8D \xF0\x9F\x91\x8D"];
+        self.likeLabel.text = likeSeries;
+    }
     
     self.questionLabel.text = question.questionText;
     
